a1a50d70199c5e8dee78f2046b9177407bd6d6e5
Shefa <shefaoudeen123@gmail.com>
2024-03-04 23:12:02 +0530

    day 15

--- DAY-15/coffeeMachine.py
+++ DAY-15/coffeeMachine.py
@@ -1,13 +1,30 @@
 import data
 
-while 1:
-    user_choice = input("What would you like? (espresso/latte/cappuccino) : ").lower()
 
+def print_report():
+    """Print the report"""
+    print(f"Water: {data.resources['water']}")
+    print(f"Milk : {data.resources['milk']}")
+    print(f"Coffee: {data.resources['coffee']}")
+    print(f"Cash: {data.resources['cash']}")
+
+
+def collect_cash():
+    """Collect cash from the customer"""
+    print("Please insert coins.")
+    quarters = int(input("How many quarters? : "))
+    dimes = int(input("How many dimes? : "))
+    nickles = int(input("How many nickles? : "))
+    pennies = int(input("How many pennies? : "))
+    total_cash = (quarters * 0.25) + (dimes * 0.1) + (nickles * 0.05) + (pennies * 0.01)
+    return total_cash
+
+
+while 1:
+
+    user_choice = input("What would you like? (espresso/latte/cappuccino) : ").lower()
     if user_choice == 'report':
-        print(f"Water: {data.resources['water']}")
-        print(f"Milk : {data.resources['milk']}")
-        print(f"Coffee: {data.resources['coffee']}")
-        print(f"Cash: {data.resources['cash']}")
+        print_report()
 
     elif user_choice == 'latte' or user_choice == 'espresso' or user_choice == 'cappuccino':
 
@@ -20,16 +37,8 @@ while 1:
                 missing_ingredients.append(element)
 
         if ingredients:
-
-            print("Please insert coins.")
-            quarters = int(input("How many quarters? : "))
-            dimes = int(input("How many dimes? : "))
-            nickles = int(input("How many nickles? : "))
-            pennies = int(input("How many pennies? : "))
-            customer_cash = (quarters * 0.25) + (dimes * 0.1) + (nickles * 0.05) + (pennies * 0.01)
-
+            customer_cash = collect_cash()
             if customer_cash >= data.MENU[user_choice]['cost']:
-
                 data.resources['cash'] += data.MENU[user_choice]['cost']
                 for element in data.MENU[user_choice]['ingredients']:
                     data.resources[element] -= data.MENU[user_choice]['ingredients'][element]
